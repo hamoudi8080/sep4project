82ef65f6b45985f47865eee26a681a85bcabc161
hamoudi8080 <hamoderaso@outlook.com>
2022-12-05 12:48:40 +0100

    hello

--- Model/Contactss/Interfaces/IMeasurement.cs
+++ Model/Contactss/Interfaces/IMeasurement.cs
@@ -2,7 +2,7 @@ namespace Model.Contactss.Interfaces;
 
 public interface IMeasurement
 {
-    
+    //hello hamoudi
     Task<Measurement> AddMeasurementAsync(Measurement measurement);
     Task<Measurement> GetMeasurementAsync(string mui);
     Task<ICollection<Measurement>> GetMeasurementHistoryAsync(string eui);
